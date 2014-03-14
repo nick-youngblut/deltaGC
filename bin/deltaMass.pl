e1ebf985e7772d4798f13b436245fb19b51ba0b9
Nick Youngblut <ndy2@cornell.edu>
2014-03-14 07:12:46 -0600

    deltaMass: GC calc done

---
+++ bin/deltaMass.pl
@@ -0,0 +1,145 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+=pod
+
+=head1 NAME
+
+deltaMass.pl -- basic description here
+
+=head1 VERSION
+
+This is version 0.0.1
+
+=head1 USAGE
+
+deltaMass.pl [options] -reads -genomes
+
+=head1 REQUIRED ARGUMENTS
+
+=over
+
+=item -r[eads]=<read_file>
+
+Reads output by grinder
+
+=for Euclid:
+read_file.type: readable
+
+=item -g[enomes]=<genome_file>
+
+Fasta file of genomes used as '-reference_file' in grinder
+
+=for Euclid:
+genome_file.type: readable
+
+=back
+
+=head1 OPTIONS
+
+=over
+
+=item -sd <size_dist> | -size_distribution <size_dist>
+
+Distribution from which fragment sizes are randomly selected.
+Options: 'uniform','normal','exponential'
+
+Default: size_dist.default
+
+=for Euclid:
+size_dist.type: string, size_dist eq 'uniform' || size_dist eq 'normal' || size_dist eq 'exponential'
+size_dist.type.error: <size_dist> must be 'uniform', 'normal', or 'exponential'
+size_dist.default: 'exponential'
+
+=item -min <min_length>
+
+Minimum fragment length.
+
+Default: min_length.default
+
+=for Euclid:
+min_length.type: int >= 0
+min_length.default: 4000
+
+=item -max <max_length>
+
+Maximum fragment length.
+
+Default max_length.default
+
+=for Euclid:
+max_length.type: int >= 0
+max_length.default: 100000
+
+=item --debug [<log_level>]
+
+Set the log level. Default is log_level.default but if you provide --debug,
+then it is log_level.opt_default.
+
+=for Euclid:
+    log_level.type:        int
+    log_level.default:     0
+    log_level.opt_default: 1
+
+=item --version
+
+=item --usage
+
+=item --help
+
+=item --man
+
+Print the usual program information
+
+=back
+
+=head1 DESCRIPTION
+
+The rest of the documentation
+
+=head1 EXAMPLES
+
+=head1 AUTHOR
+
+Nick Youngblut (ndy2@cornell.edu)
+
+=head1 BUGS
+
+There are undoubtedly serious bugs lurking somewhere in this code.
+Bug reports and other feedback are most welcome.
+
+=head1 COPYRIGHT
+
+Copyright 2010, 2011
+This software is licensed under the terms of the GPLv3
+
+=cut
+
+use Data::Dumper;
+use Getopt::Euclid;
+use FindBin qw/$Bin/;
+use lib "$Bin";
+
+use deltaMass qw/
+load_genome_fasta
+load_reads
+get_frag_GC
+/;
+
+# parsing genome file
+## genome_name => sequence
+my $genome_seqs_r = load_genome_fasta($ARGV{-genomes});
+
+# parsing reads file
+my $reads_r = load_reads($ARGV{-reads});
+
+# creating fragments for each read & calculating GC
+get_frag_GC($genome_seqs_r, $reads_r, 
+	    $ARGV{'-sd'}, $ARGV{'-min'}, $ARGV{'-max'});
+
+# calculate variance & CI
+get_GC_stats($reads_r);
+
+# writing out summary
+#write_summary($reads_r);
--- lib/deltaMass.pm
+++ lib/deltaMass.pm
@@ -6,7 +6,7 @@ use warnings;
 
 =head1 NAME
 
-deltaMass - The great new deltaMass!
+deltaMass - scripts for running deltaMass.pl
 
 =head1 VERSION
 
@@ -19,35 +19,29 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use deltaMass;
-
-    my $foo = deltaMass->new();
-    ...
-
 =head1 EXPORT
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
 
 =head1 SUBROUTINES/METHODS
 
 =cut
 
 use base 'Exporter';
-our @EXPORT_OK = qw/
-calc_GC
-/;
+our @EXPORT_OK = '';
 use Carp qw/ confess /;
 use Data::Dumper;
+use Text::ParseWords;
+use Math::Random qw/
+random_uniform_integer
+random_uniform
+random_normal
+random_exponential/;
 
 =head2 calc_GC
 
-
 =cut
+
+push @EXPORT_OK, 'calc_GC';
 
 sub calc_GC {
 # calculting GC of a sequence
@@ -63,7 +57,7 @@ sub calc_GC {
   my $len = length $seq;
 
   # GC
-  my %GC = ( G => 0, C => 0);
+  my %GC = ( G => 0, C => 0 );
   while( $seq =~ /G/gi ){
     $GC{G}++;
   }
@@ -71,21 +65,22 @@ sub calc_GC {
     $GC{C}++;
   }
   my $GC_sum = $GC{G} + $GC{C};
-
   my $GC_content = $GC_sum / $len * 100;
 
-  return $GC_content, $GC_sum, $len;
+  return $GC_content, $len;
 }
 
-=head2 load_fasta
+=head2 load_genome_fasta
 
 =cut
 
-push @EXPORT_OK, 'load_fasta';
+push @EXPORT_OK, 'load_genome_fasta';
 
-sub load_fasta {
-  my $fasta_in = shift;
-  open IN, $fasta_in or die $!;
+sub load_genome_fasta {
+  my $fasta_file = shift;
+  open IN, $fasta_file or die $!;
+
+  # loading fasta 
   my (%fasta, $tmpkey);
   while(<IN>){
     chomp;
@@ -93,15 +88,187 @@ sub load_fasta {
     next if  /^\s*$/;
     if(/>.+/){
       $tmpkey = $_;# changing key
-      $tmpkey =~ s/>| .+//g;
-      $fasta{$tmpkey} = "";
+      $tmpkey =~ s/>| .+//g; # nothing after space kept
+      $fasta{$tmpkey}{seq} = "";
     }
-    else{$fasta{$tmpkey} .= $_; }
+    else{$fasta{$tmpkey}{seq} .= $_; }
   }
   close IN;
+
+  # getting genome lengths
+  map{ $fasta{$_}{len} = length $fasta{$_}{seq} } keys %fasta;
+
   #print Dumper %fasta; exit;
   return \%fasta;
 }
+
+=head2 load_reads
+
+Loading reads produced by grinder.
+Fasta format, but headers need to be parsed
+
+=cut
+
+push @EXPORT_OK, 'load_reads';
+
+sub load_reads{
+  my $reads_file = shift;
+  
+  open IN, $reads_file or die $!;
+
+  my (%fasta, $ref, $Uid);
+  while(<IN>){
+    chomp;
+
+    if(eof(IN)){
+      $fasta{$ref}{$Uid}{seq} .= $_;
+      ($fasta{$ref}{$Uid}{amp_GC}, $fasta{$ref}{$Uid}{amp_len}) 
+	= calc_GC($fasta{$ref}{$Uid}{seq});
+      delete $fasta{$ref}{$Uid}{seq} or die $!;
+    }
+    elsif(/^>/){
+      # calculating GC on last sequence
+      if( defined $ref ){
+	($fasta{$ref}{$Uid}{amp_GC}, $fasta{$ref}{$Uid}{amp_len}) 
+	  = calc_GC($fasta{$ref}{$Uid}{seq});
+	delete $fasta{$ref}{$Uid}{seq} or die $!;
+      }
+
+      # loading new sequence
+      my @l = split />| /, $_, 3;
+      my %vals = quotewords("=| ", 0,  $l[2]);
+      
+      $Uid = $l[1];
+      $ref = $vals{reference};
+      
+      ## amplicon start-end
+      my ($amp_start, $amp_end, $amp_strand);
+      if( $vals{amplicon} =~ /complement\((.+)\)/){ 
+	($amp_end, $amp_start) = split /\.\./, $1;
+	$amp_strand = -1;
+      }
+      else{
+	($amp_start, $amp_end) = split /\.\./, $vals{amplicon};
+	$amp_strand = 1;
+      }
+      
+      $fasta{$ref}{$Uid}{seq} = "";
+      $fasta{$ref}{$Uid}{amp_start} = $amp_start;
+      $fasta{$ref}{$Uid}{amp_end} = $amp_end;
+      $fasta{$ref}{$Uid}{amp_strand} = $amp_strand;
+      $fasta{$ref}{$Uid}{desc} = $vals{description};
+    }
+    else{ # just part of the sequence
+      next if /^\s*$/;
+      $fasta{$ref}{$Uid}{seq} .= $_;
+    }
+  }
+
+  close IN or die $!;
+
+  #print Dumper %fasta; exit;
+  return \%fasta;
+}
+
+=head2 get_frag_GC
+
+creating fragments for each read & calculating GC on each
+
+=cut
+
+push @EXPORT_OK, 'get_frag_GC';
+
+sub get_frag_GC{
+# foreach read:
+## determine random fragment size
+## pull fragment from genome
+### start-end around amplicon determine from a uniform distribution
+## calculate fragment GC
+## record GC & length
+  my ($genome_seqs_r, $reads_r, $size_dist, $frag_min, $frag_max) = @_;
+
+  map{ confess "ERROR: argument missing: $!\n" unless defined $_ } 
+    ($size_dist, $frag_min, $frag_max);
+
+  # options that should be user-provided!!!
+  my $mean = 4100;
+  my $stdev = 100;
+  my $primer_buffer = 70;
+
+  # interating through fragments
+  foreach my $genome (keys %$reads_r){
+    die "ERROR: cannot find '$genome' in genomes fasta\n"
+      unless exists $genome_seqs_r->{$genome};
+
+    foreach my $Uid (keys %{$reads_r->{$genome}}){
+      my $amp_start = $reads_r->{$genome}{$Uid}{amp_start};
+      my $amp_end = $reads_r->{$genome}{$Uid}{amp_end};
+      my $amp_len = $reads_r->{$genome}{$Uid}{amp_len};
+
+      # fragment size
+      my $frag_size;
+      if($size_dist eq 'uniform'){
+       $frag_size = random_uniform_integer(1, $frag_min, $frag_max);
+      }
+      elsif($size_dist eq 'normal'){
+	while(1){
+	  $frag_size = int random_normal(1, $mean, $stdev);
+	  last if $frag_size >= $frag_min && $frag_size <= $frag_max;
+	}
+      }
+      elsif($size_dist eq 'exponential'){
+	while(1){
+	  $frag_size = int random_exponential(1, $mean);
+	  last if $frag_size >= $frag_min && $frag_size <= $frag_max;
+	}
+      }
+      else{ confess "ERROR: do not recognize size distribution\n"; }
+      
+      #print Dumper $frag_size; exit;
+      
+      # determine fragment start-end based on amplicon start-end
+      ## amplicon center postion
+      my $amp_center = int($amp_start + abs($amp_len * 0.5));
+      ## fragment_start = amp_center - (frag_size * x); x = random draw from unifrom distribution 0:1
+      my $x = random_uniform();
+      ### start_floor = (primer_buffer + 0.5*amp_len) / frag_size
+      my $floor  = ($primer_buffer + 0.5 * $amp_len) / $frag_size; 
+      $x = $floor if $x < $floor;
+      my $ceiling = 1 - $floor;
+      $x = $ceiling if $x > $ceiling;
+      my $frag_start = int( $amp_center - ($frag_size * $x) );
+
+      ### sanity check 
+      confess "ERROR: frag_start is too far from amplicon\n"
+	if $frag_start - ($amp_end + $primer_buffer) > $frag_size;
+
+      # getting fragment
+      ## wrapping around genome sequence string if neede
+      my $frag_seq;
+      if($frag_start + $frag_size + 1 > $genome_seqs_r->{$genome}{len}){ # wrap
+	my $max_len = $genome_seqs_r->{$genome}{len} - $frag_start + 1;
+	$frag_seq = substr($genome_seqs_r->{$genome}{seq}, $frag_start, $max_len);
+	$frag_seq .= substr($genome_seqs_r->{$genome}{seq}, 0, $frag_size - $max_len);
+      }
+      else{
+	$frag_seq = substr($genome_seqs_r->{$genome}{seq}, $frag_start, $frag_size );
+      }
+
+      # getting fragment GC
+      ($reads_r->{$genome}{$Uid}{frag_GC},$reads_r->{$genome}{$Uid}{frag_len}) =
+	calc_GC( $frag_seq );
+    }
+  }
+  
+  #print Dumper $reads_r; exit;
+}
+
+=head2 write_summary
+
+Summary table of GC 
+
+=cut
+
 
 =head1 AUTHOR
 
